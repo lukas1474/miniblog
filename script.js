552f63693603c0799c8025cc83ed206b9147edcb
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-06-09 16:56:20 +0200

    add voting sytem

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const onFormSubmit = () => {
     const formData = readFormData();
-    console.log('formData', formData)
     addNewPost(formData);
     clearForm();
 }
@@ -15,18 +14,33 @@ const readFormData = () => {
 const addNewPost = (dataPost) => {
     const postsList = document.getElementById("postList").getElementsByTagName("tbody")[0];
     const newRow = postsList.insertRow(postsList.length)
+
     newRow.insertAdjacentHTML(
         'beforeend',
-        `<div id="two">
+        `<div class="posts">
+            <div class="post_details">
             <h2>${dataPost.Title}</h2>
             <p>${dataPost.Text}</p>
             <button onClick="removePost(this)">Delete</button>
+            </div>
+            <div class="votes">
+                <p class="votes_p">votes</p>
+                <div class="vote_buttons">
+                    <div class="button_column">
+                        <p id="numberPlus">0</p>
+                        <button id="increment" class="vote_button">+</button>
+                    </div>
+                    <div class="button_column">
+                        <p id="numberMinus">0</p>
+                        <button id="decrement" class="vote_button">-</button>
+                    </div>
+                </div>
+            </div>
         </div>`
     )
-    // cellTitle = newRow.insertCell(0)
-    // cellTitle.innerHTML = dataPost.Title;
-    // cellText = newRow.insertCell(1)
-    // cellText.innerHTML = dataPost.Text;
+    
+    alert('success');
+    voting();
 }
 
 const clearForm = () => {
@@ -36,25 +50,21 @@ const clearForm = () => {
 
 const removePost = (post) => {
     row = post.parentElement.parentElement;
-    document.getElementById("postList").deleteRow(row.rowIndex);
+    document.getElementById('postList').deleteRow(row.rowIndex);
     clearForm();
 }
 
-
 const data = () => {
     const postList = document.getElementById('post');
-    // console.log(postList)
     fetch("data.json")
       .then(response => response.json())
       .then(data => {
-        // console.log('data', data)
         data.map(function(post){
             postList.insertAdjacentHTML(
                 'beforeend', 
-                `<div id="two">
+                `<div id="two" class="archival_post">
                     <h2>${post.title}</h2>
                     <p>${post.text}</p>
-                    <button>Delete</button>
                 </div>`
             )
         });
@@ -62,3 +72,31 @@ const data = () => {
 }
 
 data();
+
+const voting = () => {
+    const allPosts = document.querySelectorAll('.votes');
+
+    for (const singlePost of allPosts) {
+
+        const addVotePlus = singlePost.querySelector('#increment');
+        const votesValuePlus = singlePost.querySelector('#numberPlus');
+        let valuePlus = 0;
+        addVotePlus.addEventListener('click', function() {
+            valuePlus += 1;
+            votesValuePlus.innerHTML = valuePlus;
+        })
+
+        const addVoteMinus = singlePost.querySelector('#decrement');
+        const votesValueMinus = singlePost.querySelector('#numberMinus');
+        let valueMinus = 0;
+        addVoteMinus.addEventListener('click', function() {
+            valueMinus += 1;
+            votesValueMinus.innerHTML = valueMinus;
+        })
+    }
+}
+
+
+const addVoteMinus = document.getElementById('decrement');
+const votesValueMinus = document.getElementById('numberMinus');
+const valueMinus = 0;
